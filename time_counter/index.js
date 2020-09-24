2c600b9a71021a58f9fe91d50687b0519b6cef9e
spmhot <43824111+spmhot@users.noreply.github.com>
2020-09-24 10:58:50 +0530

    Update index.js

--- time_counter/index.js
+++ time_counter/index.js
@@ -1,1 +1,32 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
 
+const myBirthday = '07 Jun 2021';
+
+function x() {
+    const mybddate = new Date(myBirthday);
+    const currentDate = new Date();
+
+    const totalseconds =
+        Math.floor(new Date(mybddate) - new Date(currentDate)) / 1000;
+    const days = Math.floor(totalseconds / 3600 / 24);
+    const hours = Math.floor(totalseconds / 60) % 24;
+    const minutes = Math.floor(totalseconds / 60) % 60;
+    const seconds = Math.floor(totalseconds) % 60;
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formTime(hours);
+    minutesEl.innerHTML = formTime(minutes);
+    secondsEl.innerHTML = formTime(seconds);
+
+}
+
+function formTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+x();
+setInterval(x, 100)
+if (x == myBirthday) {
+    window.alert("hhh");
+}
